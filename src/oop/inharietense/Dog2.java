7a1051a93bf9fc18da381af61236a5b7db30d9b1
Samat <samatabdraev161@gmail.com>
2023-02-11 19:50:16 +0600

    git

--- src/oop/inharietense/Dog2.java
+++ src/oop/inharietense/Dog2.java
@@ -12,5 +12,7 @@ public class Dog2 extends Dog{
     public Dog2(String name, int age, double weight) {
         super(name, age);
         this.weight = weight;
+
+//        ghp_tIV3MKSx8DS1pVSabAmgit push -u origin mainaY0hEEP69j3FWiIo
     }
 }
